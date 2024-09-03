b10d5667a33e975312837b546d76b31adb008ba1
Quan <qhoang2373@gmail.com>
2024-09-03 16:51:00 -0700

    remove const categoryName

--- server.js
+++ server.js
@@ -70,6 +70,7 @@ const RESTAURANT = {
 app.get('/menu/:category', (req, res) => {
     const category = req.params.category;
     const menuItems = RESTAURANT.menu.filter(item => item.category === category);
+
     res.render('category.ejs', {menuItems, RESTAURANT});
 })
   
